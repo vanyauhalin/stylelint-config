3ce9eb46ec60fd3dc4994055617ebb789849fd9c
vanyauhalin <vanyauhalin@gmail.com>
2022-04-27 16:06:18 +0300

    chore: add support cjs for esbuild

--- .esbuildrc.js
+++ .esbuildrc.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { build } from 'esbuild';
 
 function getOptions() {
@@ -12,12 +13,16 @@ function getOptions() {
 }
 
 const { outfile, root } = getOptions();
+const src = `${root}/src/`;
+const entryPoints = fs.readdirSync(src)
+  .filter((file) => file.endsWith('.cjs'))
+  .map((file) => `${src}${file}`);
 
 build({
+  entryPoints,
   outfile,
   allowOverwrite: true,
   bundle: true,
-  entryPoints: [`${root}/src`],
   platform: 'node',
 }).catch((error) => {
   throw new Error(error.stderr);
