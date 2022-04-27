2f83c1eb1a573b7cce618af754c05617affaaf8a
vanyauhalin <vanyauhalin@gmail.com>
2022-04-27 20:25:07 +0300

    feat: add main base package file

---
+++ packages/stylelint-config-base/src/index.cjs
@@ -0,0 +1,37 @@
+/**
+ * This package is written in commonjs,
+ * because in the process of building
+ * the package on modules it gives an error:
+ * `TypeError: Cannot set property plugins
+ * of #<Object> which has only a getter`.
+ * @see https://github.com/evanw/esbuild/issues/587
+ */
+
+/**
+ * @typedef {import('stylelint').Config} Config
+ */
+
+const { rules } = require('./rules/index.cjs');
+
+/**
+ * Main Stylelint configuration whose properties will be exported.
+ * @type {Config}
+ */
+const config = {
+  plugins: 'stylelint-order',
+  overrides: [],
+  /**
+   * The `rules` property must be in the config,
+   * otherwise stylelint will throw an error:
+   * `Error: No rules found within configuration.
+   * Have you provided a 'rules' property?`
+   */
+  rules: {},
+};
+
+config.overrides.push({
+  rules,
+  files: '**/*.css',
+});
+
+module.exports = config;
