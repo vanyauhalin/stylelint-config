5b68892ccd6596c1661eca658cc8c471f02bab72
vanyauhalin <vanyauhalin@gmail.com>
2022-04-28 10:39:48 +0300

    refactor: simplify read fn

--- .esbuildrc.js
+++ .esbuildrc.js
@@ -1,46 +1,36 @@
 import fs from 'fs';
 import { build } from 'esbuild';
 
-const PACKAGES_DIRECTORY = 'packages';
-const PACKAGE_PREFIX = 'stylelint-config-';
-const PACKAGE_SOURCE_DIRECTORY = 'src';
-const PACKAGE_MAIN_FILE = 'lib/index.cjs';
-
 function read() {
+  const PACKAGES_DIRECTORY = 'packages';
   const packages = fs.readdirSync(PACKAGES_DIRECTORY);
-  const [, , name] = process.argv;
-  const pack = `${PACKAGE_PREFIX}${name}`;
-  const hasPackage = packages.includes(pack);
+  return packages.map((pack) => {
+    const root = `${PACKAGES_DIRECTORY}/${pack}`;
+    const main = `${root}/lib/index.cjs`;
+    const src = `${root}/src`;
+    return {
+      main,
+      src,
+    };
+  });
+}
 
-  if (!hasPackage) {
-    throw new Error(`Package ${name} (${pack}) not found`);
-  }
-
-  const root = `${PACKAGES_DIRECTORY}/${pack}`;
-  const src = `${root}/${PACKAGE_SOURCE_DIRECTORY}`;
-  const main = `${root}/${PACKAGE_MAIN_FILE}`;
+const packages = read();
+packages.forEach((pack) => {
+  const { main, src } = pack;
   const entries = fs.readdirSync(src)
     .filter((file) => file.endsWith('.cjs'))
     .map((file) => `${src}/${file}`);
 
-  return {
-    entries,
-    main,
-    name,
-    root,
-  };
-}
-
-const { entries, main } = read();
-
-build({
-  allowOverwrite: true,
-  bundle: true,
-  entryPoints: entries,
-  outfile: main,
-  platform: 'node',
-}).catch((error) => {
-  throw new Error(error.stderr);
+  build({
+    allowOverwrite: true,
+    bundle: true,
+    entryPoints: entries,
+    outfile: main,
+    platform: 'node',
+  }).catch((error) => {
+    throw new Error(error.stderr);
+  });
 });
 
 export {
--- test/validate.js
+++ test/validate.js
@@ -3,15 +3,17 @@ import { test } from 'uvu';
 import { equal } from 'uvu/assert';
 import { read } from '../.esbuildrc';
 
-const { main } = read();
-
-test('stylelint should throw an error', async () => {
-  const { results } = await stylelint.lint({
-    config: {
-      extends: main,
-    },
-    files: './test/*.css',
+const packages = read();
+packages.forEach((pack) => {
+  const { main } = pack;
+  test('stylelint should throw an error', async () => {
+    const { results } = await stylelint.lint({
+      config: {
+        extends: main,
+      },
+      files: './test/*.css',
+    });
+    const hasError = !!results.find((el) => el.errored);
+    equal(hasError, true);
   });
-  const hasError = !!results.find((el) => el.errored);
-  equal(hasError, true);
 });
