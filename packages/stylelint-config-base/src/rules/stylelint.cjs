f1a072fbcc97e37bedef989bbeb64af8cdb7bde3
vanyauhalin <vanyauhalin@gmail.com>
2022-05-04 22:16:58 +0300

    fix: except and ignore for comment-empty-line-before

--- packages/stylelint-config-base/src/rules/stylelint.cjs
+++ packages/stylelint-config-base/src/rules/stylelint.cjs
@@ -328,6 +328,9 @@ const rules = {
   'comment-empty-line-before': ['always', {
     except: [
       'first-nested',
+    ],
+    ignore: [
+      'after-comment',
       'stylelint-commands',
     ],
   }],
