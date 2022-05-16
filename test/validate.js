0a2a93d57c0f84c6cb70da8186d1b921a62cf255
vanyauhalin <vanyauhalin@gmail.com>
2022-05-16 14:13:41 +0300

    build: remove esbuild

--- test/validate.js
+++ test/validate.js
@@ -1,7 +1,22 @@
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
 import stylelint from 'stylelint';
 import { test } from 'uvu';
 import { is } from 'uvu/assert';
-import { read } from '../.esbuildrc';
+
+function read() {
+  const PACKAGES_DIRECTORY = 'packages';
+  const packages = readdirSync(PACKAGES_DIRECTORY);
+  return packages.map((pack) => {
+    const root = `${PACKAGES_DIRECTORY}/${pack}`;
+    const main = resolve(`${root}/lib/index.cjs`);
+    const src = resolve(`${root}/src`);
+    return {
+      main,
+      src,
+    };
+  });
+}
 
 test('stylelint should throw an configs error', async () => {
   await Promise.all(read().map(async (pack) => {
